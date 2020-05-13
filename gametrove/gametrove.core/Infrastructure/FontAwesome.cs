7ae8e788065cfdfbbe2bca2d6e46ca077b0e29da
Terry Burns-Dyson <terrybd@gmail.com>
2020-05-12 21:10:30 -0400

    Icons on flyout menu

--- gametrove/gametrove.core/Infrastructure/FontAwesome.cs
+++ gametrove/gametrove.core/Infrastructure/FontAwesome.cs
@@ -3,6 +3,7 @@
     public class FontAwesome
     {
         public const string Add = "\uf0fe";
+        public const string Cog = "\uf013";
         public const string Edit = "\uf044";
         public const string Photo = "\uf030";
         public const string Scan = "\uf02a";
